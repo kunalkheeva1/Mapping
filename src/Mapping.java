f92b2e95cb1bb608e58117019611f952bea2b26f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-16 19:06:49 -0700

    Initial commit

---
+++ src/Mapping.java
@@ -0,0 +1,21 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Mapping {
+    public static void main(String[] args) {
+        Map<Integer, String > map = new HashMap<>();
+        map.put(1,"Kunal");
+        map.put(2, "Kheeva");
+        map.put(3, "Nav");
+    
+
+    for(int i=1; i<3; i++){
+        System.out.print(map.keySet()+" :");
+
+    }
+
+
+    }
+}
